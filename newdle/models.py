b255e4efcb52247197d2e1eb1f527e7ffbb6223d
Cristian Schuszter <cristian.schuszter@cern.ch>
2020-09-24 11:46:43 +0200

    Add SQL changes workflow

--- newdle/models.py
+++ newdle/models.py
@@ -41,6 +41,7 @@ class Newdle(db.Model):
     creator_uid = db.Column(db.String, nullable=False, index=True)
     creator_name = db.Column(db.String, nullable=False)
     creator_email = db.Column(db.String, nullable=False, server_default='')
+    test_column = db.Column(db.String, nullable=False, server_default='')
     title = db.Column(db.String, nullable=False)
     duration = db.Column(db.Interval, nullable=False)
     timezone = db.Column(db.String, nullable=False)
